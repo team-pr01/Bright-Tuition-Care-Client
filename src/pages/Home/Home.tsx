c6a1e65b5629fdf79bd11ab8dafce38d91c6520a
prernabadwane <prernabadwane@gmail.com>
2025-07-06 07:12:34 +0530

    tailwind css, index.css setuped properly

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="flex h-screen w-full items-center justify-center bg-red-500 text-white">
+        <div className="flex h-screen w-full items-center justify-center >
             home
         </div>
     );
