bb90d7c03db57e74454ee47f692245beba51b270
rahulsd380 <rahulsd380@gmail.com>
2025-08-09 12:51:46 +0600

    Added tick mark in role tab button

--- src/components/AuthPage/SignupPage/SignupForm/RoleTab.tsx
+++ src/components/AuthPage/SignupPage/SignupForm/RoleTab.tsx
@@ -1,8 +1,14 @@
 import { IMAGES } from "../../../../assets";
+import { FaCheck } from "react-icons/fa";
 
-
-const RoleTab = ({ activeTab, setActiveTab } : { activeTab: string; setActiveTab: React.Dispatch<React.SetStateAction<string>>;}) => {
-      const roles = [
+const RoleTab = ({
+  activeTab,
+  setActiveTab,
+}: {
+  activeTab: string;
+  setActiveTab: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const roles = [
     {
       title: "Guardian/Student",
       description: "Get started as a Guardian or Student.",
@@ -14,28 +20,42 @@ const RoleTab = ({ activeTab, setActiveTab } : { activeTab: string; setActiveTab
       icon: IMAGES.teacher,
     },
   ];
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6">
-      {roles?.map((role) => (
-        <button
-          key={role?.title}
-          onClick={() => setActiveTab(role?.title)}
-          type="button"
-          className={` border  py-2 px-3 rounded-xl text-start flex items-center justify-between gap-6 cursor-pointer transform duration-300 ${
-            role?.title === activeTab
-              ? "bg-primary-10 border-primary-10 text-white"
-              : "bg-white border-neutral-45/20 text-neutral-10 hover:bg-neutral-50/40"
-          }`}
-        >
-          <div>
-            <h1 className="font-bold">{role?.title}</h1>
-            <p className="text-sm mt-[3px]">{role?.description}</p>
-          </div>
-          <div className="size-16 rounded-full bg-neutral-20/10 flex items-center justify-center">
-            <img src={role?.icon} alt="" className="size-11" />
-          </div>
-        </button>
-      ))}
+      {roles?.map((role) => {
+        const isActive = role?.title === activeTab;
+        return (
+          <button
+            key={role?.title}
+            onClick={() => setActiveTab(role?.title)}
+            type="button"
+            className={`relative border py-2 px-3 rounded-xl text-start flex items-center justify-between gap-6 cursor-pointer transform duration-300 ${
+              isActive
+                ? "bg-primary-10 border-primary-10 text-white"
+                : "bg-white border-neutral-45/20 text-neutral-10 hover:bg-neutral-50/40"
+            }`}
+          >
+            {/* Tick mark positioned above */}
+            {isActive && (
+              <div className="size-6 rounded-full bg-primary-10 border border-white flex items-center justify-center absolute -top-3 -right-2 ">
+                <FaCheck
+                  className="transform text-white text-xs"
+                  title="Selected"
+                />
+              </div>
+            )}
+
+            <div>
+              <h1 className="font-bold">{role?.title}</h1>
+              <p className="text-sm mt-[3px]">{role?.description}</p>
+            </div>
+            <div className="size-16 rounded-full bg-neutral-20/10 flex items-center justify-center">
+              <img src={role?.icon} alt="" className="size-11" />
+            </div>
+          </button>
+        );
+      })}
     </div>
   );
 };
